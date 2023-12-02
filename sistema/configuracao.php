fe0abb638599ffddfaa9878922e81120d75899c4
Fernando aguiar <fernando2015.aguiar@gmail.com>
2023-12-02 15:42:50 -0300

    project-loja-informatica

---
+++ sistema/configuracao.php
@@ -0,0 +1,14 @@
+<?php
+
+//Arquivo de configuração do sistema
+//define o fuso horario
+date_default_timezone_set('America/Sao_Paulo');
+
+//informações do sistema
+define('SITE_NOME', 'Garagem da informática');
+define('SITE_DESCRICAO', 'Transformando ideias em realidade!');
+
+//urls do sistema
+define('URL_PRODUCAO', 'https://garagemdainformatica.com.br');
+define('URL_DESENVOLVIMENTO', 'http://localhost/blog');
+
